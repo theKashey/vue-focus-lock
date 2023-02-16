ffd8a366bf764462cfe0304b4a651db07ccc1eb1
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2023-02-16 12:21:55 -0800

    fix: externalize vue-demi

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 
 module.exports = {
   entry: './src/index.js',
+  externals: 'vue-demi',
   output: {
     path: path.resolve(__dirname, './dist/'),
     filename: 'index.js',
