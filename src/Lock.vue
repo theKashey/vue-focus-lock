theKashey/vue-focus-lock

commit 0da44b0917fb53e259d5909114293942678c802d

typeguard mount/unmount

src/Lock.vue
@@ -148,7 +148,11 @@
       let originalFocusedElement;
 
       onMounted(() => {
-        data.value.vue = (getCurrentInstance() || {}).proxy;
+        const currentInstance = getCurrentInstance();
+        if (!currentInstance) {
+          return;
+        }
+        data.value.instance = currentInstance.proxy;
         data.value.observed = rootEl.value.querySelector("[data-lock]");
 
         data.value.disabled = disabled.value;
@@ -164,7 +168,11 @@
       });
 
       onUnmounted(() => {
-        instances = instances.filter(({vue}) => vue !== (getCurrentInstance() || {}).proxy);
+        const currentInstance = getCurrentInstance();
+        if (!currentInstance) {
+          return;
+        }
+        instances = instances.filter(({instance}) => instance !== currentInstance);
         if (!instances.length) {
           detachHandler();
         }

commit f1357beed26efa2ced22c3c9ad2f944388bbee40

Fix bug introduced in commit 0da44b0

src/Lock.vue
@@ -184,7 +184,7 @@
           return;
         }
 
-        instances = instances.filter(({instance}) => instance !== currentInstance);
+        instances = instances.filter(({instance}) => instance !== currentInstance.proxy);
 
         if (!instances.length) {
           detachHandler();